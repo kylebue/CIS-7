6dca117d853c34cc344feaa27a864cdee22ea935
Kyle Bue <kylebue9@gmail.com>
2017-10-11 16:22:14 -0700

    Updated Assignment 13

--- Assignments/Assignment 13.cs
+++ Assignments/Assignment 13.cs
@@ -1,28 +1,65 @@
 using System;
 using System.Math;
+using System.Collections.Generic;
 
 namespace HelpMe
 {
+  public class Tower
+  {
+    public string name;
+    public List<double> disks = new List<double>();
+    public Tower(string name, List<double> disks)
+    {
+      name = this.name;
+      disks = this.disks;
+    }
+    
+    public void print()
+    {
+      
+    }
+  }
+  
   class Hello 
   {
     static void Main() 
     {
-      double n = 3;
-      string a = "Start";
-      string b = "End";
-      string c = "Aux";
+      double n = 2;
+      string name = "";
+      List<double> disks = new List<double>();
+      Tower startTower = new Tower(name, disks);
+      Tower endTower = new Tower(name, disks);
+      Tower auxTower = new Tower(name, disks);
+
+      startTower.name = "Start";
+      endTower.name = "End";
+      auxTower.name = "Spare";
       
-      Move(n, a, b, c);
-      Console.WriteLine(Formula(n));
+      for (int i = 1; i <= n; i++)
+      {
+        startTower.disks.Add(i);
+      }
+      
+      startTower.print();
+      endTower.print();
+      auxTower.print();
+      
+      Move(n, startTower, endTower, auxTower);
+      //Console.WriteLine(Formula(n));
     }
     
-    static void Move(double n, string a, string b, string c)
+    static void Move(double n, Tower a, Tower b, Tower c)
     {
       if (n > 0)
       {
         Move(n-1, a, c, b);
         
-        Console.WriteLine("Move disk from " + a + " to " + b);
+        Console.WriteLine("Move disk from " + a.name + " to " + b.name);
+
+        a.print();
+        b.print();
+        c.print();
+        
         Move(n-1, c, b, a);
       }
     }
